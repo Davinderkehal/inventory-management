12bec0ddebd1eb8ba1f05e9f4707739106cd223e
DavinderKehal <kehalds@gmail.com>
2024-12-12 00:17:54 -0500

    Add ProductProperties class with methods

---
+++ app.js
@@ -0,0 +1,21 @@
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+
+    toString() {
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+    }
+
+    static applyDiscount(products, discount) {
+        products.forEach(product => {
+            product.price *= (1 - discount);
+        });
+    }
+}
