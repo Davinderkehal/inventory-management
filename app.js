605fbeb53c111d14f20424a3b2e6829b572c57a4
DavinderKehal <kehalds@gmail.com>
2024-12-12 00:33:07 -0500

    Add PerishableProductProperties subclass with expirationDate

--- app.js
+++ app.js
@@ -19,3 +19,30 @@ class ProductProperties {
         });
     }
 }
+class PerishableProductProperties extends ProductProperties {
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    toString() {
+        return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
+    }
+}
+class Store {
+    constructor() {
+        this.inventory = [];
+    }
+
+    addProduct(product) {
+        this.inventory.push(product);
+    }
+
+    getInventoryValue() {
+        return this.inventory.reduce((total, product) => total + product.getTotalValue(), 0);
+    }
+
+    findProductByName(name) {
+        return this.inventory.find(product => product.name === name) || null;
+    }
+}
